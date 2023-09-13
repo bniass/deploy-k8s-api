cc4dd59a03e5ff61dc6707832cf479a67f239be9
bniass <bniass@gmail.com>
2023-09-13 12:41:29 +0000

    third4 commit

--- src/main/java/com/ecole221/depoyk8sapi/DepoyK8sApiApplication.java
+++ src/main/java/com/ecole221/depoyk8sapi/DepoyK8sApiApplication.java
@@ -2,12 +2,19 @@ package com.ecole221.depoyk8sapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DepoyK8sApiApplication {
+public class DepoyK8sApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DepoyK8sApiApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DepoyK8sApiApplication.class);
     }
 
 }
