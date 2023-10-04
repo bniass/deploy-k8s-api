22e6a0dcb0b120341654fee62383bc8c70788a12
bniass <bniass@gmail.com>
2023-10-04 11:50:22 +0000

    ffff

--- src/test/java/com/ecole221/depoyk8sapi/DepoyK8sApiApplicationTests.java
+++ src/test/java/com/ecole221/depoyk8sapi/DepoyK8sApiApplicationTests.java
@@ -1,5 +1,7 @@
 package com.ecole221.depoyk8sapi;
 
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class DepoyK8sApiApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void sample(){
+        Assertions.assertEquals(1, 1);
     }
 
 }
